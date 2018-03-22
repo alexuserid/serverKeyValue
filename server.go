53dd871fc3e9fc71b11eb898515c7d046ec3d14e
Alex <34424602+alexuserid@users.noreply.github.com>
2018-03-22 13:46:53 +0300

    Update server.go

--- server.go
+++ server.go
@@ -24,7 +24,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	key := reKeyNums.FindString(r.URL.Path)
 
 	if r.URL.Path[1:] == "" {
-		//sending text when address bar is like site.com/
+		//sending text when url in address bar is like site.com/
 		fmt.Fprintf(w, "Enter key and value in form: sitename.com/key:value/. \nA key must compile 1-10 integers, a value must compile 1-15 strings. \nIf a pair key:value is already exis, you can get the value by key, enter sitename.com/key:/")
 
 	} else if validKV.FindString(r.URL.Path) != "" { //if key and value are both correct
@@ -43,7 +43,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 				fmt.Fprintf(w, "The value for key %s is %s", key, v.Value) //text with value
 			}
 		}
-		fmt.Fprintf(w, "Theres no value for this key") //text if value isnt present
+		fmt.Fprintf(w, "There's no value for this key") //text if value isnt present
 
 	} else {
 		fmt.Fprintf(w, "The request is incorrect") //text if request is incorrect
