326ead6f5f48efb4d6df6b8887cafb6bd5a2e42a
Alex <34424602+alexuserid@users.noreply.github.com>
2018-03-23 15:56:05 +0300

    Update server.go

--- server.go
+++ server.go
@@ -10,49 +10,51 @@ import (
 type KV struct {
 	Key   string
 	Value string
-}
+} 
 
-var data = []KV{}
+var (
+	data []KV
+
+	validKV = regexp.MustCompile("/[0-9]{1,10}:[a-zA-Z]{1,15}/")
+	reKey = regexp.MustCompile("/[0-9]{1,10}:")
+	reValue = regexp.MustCompile("[a-zA-Z]{1,15}")
+	reKeyNums = regexp.MustCompile("[0-9]{1,10}")
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	validKV := regexp.MustCompile("/[0-9]{1,10}:[a-zA-Z]{1,15}/")
-	reKey := regexp.MustCompile("/[0-9]{1,10}:")
-	reValue := regexp.MustCompile("[a-zA-Z]{1,15}")
-	reKeyNums := regexp.MustCompile("[0-9]{1,10}")
-
 	key := reKeyNums.FindString(r.URL.Path)
 
-	if r.URL.Path[1:] == "" {
+	if r.URL.Path == "/" {
 		//sending text when url in address bar is like site.com/
-		fmt.Fprintf(w, "Enter key and value in form: sitename.com/key:value/. \nA key must contain 1-10 integers, a value must contain 1-15 strings. \nIf a pair key:value is already exis, you can get the value by key, enter sitename.com/key:/")
+		w.Write([]byte("Enter key and value in form: sitename.com/key:value/. \nA key must contain 1-10 integers, a value must contain 1-15 strings. \nIf a pair key:value is already exis, you can get the value by key, enter sitename.com/key:/"))
 
 	} else if validKV.FindString(r.URL.Path) != "" { //if key and value are both correct
-		for i, v := range data { //ranging data with already exesting pairs
-			if v.Key == key { //if match is present
+		for i := range data { //ranging data with already exesting pairs
+			if data[i].Key == key { //if match is present
 				data[i].Value = reValue.FindString(r.URL.Path)           //rewriting value to existing key
 				fmt.Fprintf(w, "The value for key %s was rewrited", key)
+				return
 			}
 		}
 		data = append(data, KV{key, reValue.FindString(r.URL.Path)}) //adding pair to data
-		fmt.Fprintf(w, "The pair was added")                       
-
+		w.Write([]byte("The pair was added"))
+                     
 	} else if reKey.FindString(r.URL.Path) != "" { //if path include only key like /ints:
-		for _, v := range data { //ranging data with already exesting pairs
-			if v.Key == key { //if match is present
-				fmt.Fprintf(w, "The value for key %s is %s", key, v.Value)
+		for i := range data { //ranging data with already exesting pairs
+			if data[i].Key == key { //if match is present
+				fmt.Fprintf(w, "The value for key %s is %s", key, data[i].Value)
+				return
 			}
 		}
-		fmt.Fprintf(w, "There's no value for this key") //text if value isnt present
+		w.Write([]byte("There's no value for this key")) //text if value isnt present
 
 	} else {
-		fmt.Fprintf(w, "The request is incorrect") //text if request is incorrect
+		w.Write([]byte("The request is incorrect")) //text if request is incorrect
 	}
 }
 
 func main() {
-	data = append(data, KV{"", ""})
-
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
